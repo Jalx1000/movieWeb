651d6b0f5e59366e26272af21d6a0648c06c3a60
JAL <jalejandromogro1@gmail.com>
2022-07-14 03:17:39 -0400

    route acabadas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons-.min.css'
 import './App.scss';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+
+import RouteS from './config/Routes'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Route render={props=>(
+        <>
+          <Header {...props}/>        
+        </>
+      )} />
+    </BrowserRouter>
   );
 }
 
--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,4 +1,4 @@
-const apiConfig={
+const apiConfig = {
   baseUrl: 'http://api.themoviedb.org/3/',
   apiKey:'1cc28d7cb8202fa7566afa90c4a8b9f4',
   originalImage:(imagePath)=>`htts://image.tmdb.org/t/p/original/${imagePath}`,
